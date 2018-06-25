241592362ac09befe3b4b0b8efadfe7c56ec1d5c
anna.hirai <tomato@gmail.com>
2018-06-25 03:32:11 +0000

    fix

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,4 +1,4 @@
-@if (Auth::id() != $user->id)
+
     @if (Auth::user()->is_favorite($micropost->id))
         {!! Form::open(['route' => ['user.unfavorite', $micropost->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
@@ -7,5 +7,4 @@
         {!! Form::open(['route' => ['user.favorite', $micropost->id]]) !!}
             {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
         {!! Form::close() !!}
-    @endif
 @endif
